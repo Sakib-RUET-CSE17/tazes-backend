ef3718f7c2f41050069af05fd6bbf06df515e526
Sakib-RUET-CSE17 <hsakib8685@gmail.com>
2022-10-13 20:44:02 +0600

    reset-password added

--- config/bundles.php
+++ config/bundles.php
@@ -22,4 +22,5 @@ return [
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
