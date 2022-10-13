70d318beac68b1b5dfccc464c07750f3285f6f62
Sakib-RUET-CSE17 <hsakib8685@gmail.com>
2022-10-13 17:16:40 +0600

    shell command added

---
+++ refreshDatabase.sh
@@ -0,0 +1,7 @@
+symfony console doctrine:database:drop --force
+
+symfony console doctrine:database:create
+
+symfony console doctrine:schema:update --force
+
+symfony console doctrine:fixture:load -n
